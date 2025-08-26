01b0e285427089c85199f5f6487d7ff83984ce5b
MaryK <marykanyingi01@gmail.com>
2025-08-26 19:30:08 +0800

    Authentication

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from .views import CustomUserList, CustomUserDetail
 
 urlpatterns = [
-    path('users/',Views.CustomUserList.as_view()),
+    path('users/', CustomUserList.as_view()),
     path('users/<int:pk>/', CustomUserDetail.as_view()),
 ]
